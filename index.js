342498f914b529a9f93e32a0fcb90d2a959d79aa
Willem Bouma <wbouma@ilionx.com>
2017-04-03 16:01:49 +0200

    Remove index.js in root and webpack from dependencies

--- index.js
+++
@@ -1,8 +0,0 @@
-var PacktPub = require('./PacktPub/index')
-var MythicSpoilerUpdate = require('./MythicSpoilerUpdate/index')
-
-// HaloGuardians can't be packed yet because of problems with packing Zombie.
-module.exports = {
-    PacktPub: PacktPub,
-    MythicSpoilerUpdate: MythicSpoilerUpdate
-}
